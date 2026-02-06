e202006f7e5f4761a5247d29f23a51f2f5383e9f
Shifali Rathore <shifalirathore350@gmail.com>
2026-02-06 21:18:14 +0530

    Add request interceptor and API traffic logging

---
+++ src/main/java/com/apiguard/api_guard/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.apiguard.api_guard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "API Guard is watching 👀";
+    }
+}
